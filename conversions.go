103cfd6ce43c3adce64344c355cfd0c774a2a3f7
Joe Gillotti <joe@u13.net>
2021-07-09 15:43:45 +0000

    Replace more unnecessary bitwise ops with encoding/binary

--- conversions.go
+++ conversions.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/binary"
 	"net"
 )
@@ -11,28 +10,15 @@ func ip2long(ip net.IP) uint32 {
 	return binary.BigEndian.Uint32(ip.To4()[0:4])
 }
 
-func long2ip(ipIntLong uint32) net.IP {
-	ipInt := int64(ipIntLong)
-	b0 := byte(ipInt >> 24 & 0xff)
-	b1 := byte(ipInt >> 16 & 0xff)
-	b2 := byte(ipInt >> 8 & 0xff)
-	b3 := byte(ipInt & 0xff)
-	return net.IP{b0, b1, b2, b3}
+func long2ip(data uint32) net.IP {
+	b := long2bytes(data)
+	return net.IP{b[0], b[1], b[2], b[3]}
 }
 
-// Quickly ripped from https://gist.github.com/chiro-hiro/2674626cebbcb5a676355b7aaac4972d
-func long2bytes(ip uint32) []byte {
-	r := make([]byte, 4)
-	for i := uint32(0); i < 4; i++ {
-		r[3-i] = byte((ip >> (8 * i)) & 0xff)
-	}
-	return r
-}
-
-func bytes2long(ip []byte) uint32 {
-	var long uint32
-	binary.Read(bytes.NewBuffer(ip), binary.LittleEndian, &long)
-	return long
+func long2bytes(data uint32) []byte {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, data)
+	return b
 }
 
 func calcBroadcast(network, netmask net.IP) net.IP {
